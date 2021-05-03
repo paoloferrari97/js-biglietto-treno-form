f81facf641368266ffb632cebc766cbd7c3f198f
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-03 16:45:53 +0200

    Creo funzione pulsante Genera

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-var nomeUtente = document.querySelector("#nome_cognome").value;
-var km = document.querySelector("#km").value;
-var età = document.querySelector("#fascia_età").value;
+var nomeUtente = "";
+var km = 0;
+var età = "medio";
 
 function annullare() {
     document.querySelector("#nome_cognome").value = "";
@@ -8,4 +8,14 @@ function annullare() {
     età = document.querySelector("#fascia_età").value = "medio";
 }
 
+function generare() {
+    nomeUtente = document.querySelector("#nome_cognome").value;
+    km = document.querySelector("#km").value;
+    età = document.querySelector("#fascia_età").value;
+
+    console.log(nomeUtente, km, età);
+}
+
 document.getElementById("annulla").addEventListener('click', annullare());
+
+document.getElementById("genera").addEventListener("click", generare());
