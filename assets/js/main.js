0284b209d2a76e13aa4f2eb332d7c1e1c75ce506
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-03 16:40:50 +0200

    Creo funzione pulsante annulla JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 var nomeUtente = document.querySelector("#nome_cognome").value;
 var km = document.querySelector("#km").value;
+var età = document.querySelector("#fascia_età").value;
+
+function annullare() {
+    document.querySelector("#nome_cognome").value = "";
+    km = document.querySelector("#km").value = "";
+    età = document.querySelector("#fascia_età").value = "medio";
+}
+
+document.getElementById("annulla").addEventListener('click', annullare());
