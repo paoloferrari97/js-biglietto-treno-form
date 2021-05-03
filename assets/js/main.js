4af1c13ca2fc51a741ac2edac09c82aa806a36b1
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-03 17:32:50 +0200

    Termino js con gli altri dati da inserire a video

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,9 @@ function annullare() {
 
     document.getElementById("nome_utente").innerHTML = "";
     document.getElementById("costo_biglietto").innerHTML = "";
+    document.getElementById("offerta").innerHTML = "";
+    document.getElementById("carrozza").innerHTML = "";
+    document.getElementById("codice_treno").innerHTML = "";
 }
 
 function generare() {
@@ -22,20 +25,30 @@ function generare() {
     document.getElementById("nome_utente").innerHTML = nomeUtente;
 
     //prezzo biglietto e se minorenni sconto 20%, se over 65 sconto 40%
+    //e offerta
     var prezzo = 0.21 * km;
     var prezzoFinale;
     if (età == "minorenne") {
         prezzoFinale = (prezzo * 80) / 100;
         document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+        document.getElementById("offerta").innerHTML = "Sconto per minorenni";
     } else if (età == "over") {
         prezzoFinale = (prezzo * 60) / 100;
         document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+        document.getElementById("offerta").innerHTML = "Sconto per over 60";
     } else {
         prezzoFinale = prezzo;
         document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+        document.getElementById("offerta").innerHTML = "Non hai diritto a sconti";
     }
 
-    //
+    //carrozza
+    var numeroCarrozza = Math.ceil(Math.random() * 50);
+    document.getElementById("carrozza").innerHTML = numeroCarrozza;
+
+    //codice treno
+    var codiceTreno = Math.floor(Math.random() * (999999 - 90000 + 1) + 90000);
+    document.getElementById("codice_treno").innerHTML = codiceTreno;
 }
 
 document.getElementById("annulla").addEventListener('click', annullare);
