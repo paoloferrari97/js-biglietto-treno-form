87f85f52d3a6d952f1b658da1219f475153b45d8
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-03 19:10:55 +0200

    Sistemo js e aggiungo controlli per dati in input

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,37 +18,41 @@ function generare() {
     nomeUtente = document.querySelector("#nome_cognome").value.toLowerCase();
     km = document.querySelector("#km").value;
     età = document.querySelector("#fascia_età").value;
-    console.log(nomeUtente, km, età);
 
-
-    //nome utente
-    document.getElementById("nome_utente").innerHTML = nomeUtente;
-
-    //prezzo biglietto e se minorenni sconto 20%, se over 65 sconto 40%
-    //e offerta
-    var prezzo = 0.21 * km;
-    var prezzoFinale;
-    if (età == "minorenne") {
-        prezzoFinale = (prezzo * 80) / 100;
-        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
-        document.getElementById("offerta").innerHTML = "Sconto per minorenni";
-    } else if (età == "over") {
-        prezzoFinale = (prezzo * 60) / 100;
-        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
-        document.getElementById("offerta").innerHTML = "Sconto per over 60";
+    if (nomeUtente == "" || isNaN(nomeUtente) == false) {
+        alert("Inserisci il tuo nome e cognome in maniera corretta!");
+    } else if (isNaN(km) || km == 0) {
+        alert("Inserisci i km da percorrere in maniera corretta!");
     } else {
-        prezzoFinale = prezzo;
-        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
-        document.getElementById("offerta").innerHTML = "Non hai diritto a sconti";
+        //nome utente
+        document.getElementById("nome_utente").innerHTML = nomeUtente;
+
+        //prezzo biglietto e se minorenni sconto 20%, se over 65 sconto 40%
+        //e offerta
+        var prezzo = 0.21 * km;
+        var prezzoFinale;
+        if (età == "minorenne") {
+            prezzoFinale = (prezzo * 80) / 100;
+            document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+            document.getElementById("offerta").innerHTML = "Sconto per minorenni";
+        } else if (età == "over") {
+            prezzoFinale = (prezzo * 60) / 100;
+            document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+            document.getElementById("offerta").innerHTML = "Sconto per over 60";
+        } else {
+            prezzoFinale = prezzo;
+            document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+            document.getElementById("offerta").innerHTML = "Non hai diritto a sconti";
+        }
+
+        //carrozza
+        var numeroCarrozza = Math.ceil(Math.random() * 50);
+        document.getElementById("carrozza").innerHTML = numeroCarrozza;
+
+        //codice treno
+        var codiceTreno = Math.floor(Math.random() * (99999 - 90000 + 1) + 90000);
+        document.getElementById("codice_treno").innerHTML = codiceTreno;
     }
-
-    //carrozza
-    var numeroCarrozza = Math.ceil(Math.random() * 50);
-    document.getElementById("carrozza").innerHTML = numeroCarrozza;
-
-    //codice treno
-    var codiceTreno = Math.floor(Math.random() * (999999 - 90000 + 1) + 90000);
-    document.getElementById("codice_treno").innerHTML = codiceTreno;
 }
 
 document.getElementById("annulla").addEventListener('click', annullare);
