a5532e91ecf78791e5f2caacb09a1f8d15175555
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-03 17:19:51 +0200

    Sistemo js, creo parte del calcolo prezzo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,38 @@ function annullare() {
     document.querySelector("#nome_cognome").value = "";
     km = document.querySelector("#km").value = "";
     età = document.querySelector("#fascia_età").value = "medio";
+
+    document.getElementById("nome_utente").innerHTML = "";
+    document.getElementById("costo_biglietto").innerHTML = "";
 }
 
 function generare() {
-    nomeUtente = document.querySelector("#nome_cognome").value;
+    nomeUtente = document.querySelector("#nome_cognome").value.toLowerCase();
     km = document.querySelector("#km").value;
     età = document.querySelector("#fascia_età").value;
-
     console.log(nomeUtente, km, età);
+
+
+    //nome utente
+    document.getElementById("nome_utente").innerHTML = nomeUtente;
+
+    //prezzo biglietto e se minorenni sconto 20%, se over 65 sconto 40%
+    var prezzo = 0.21 * km;
+    var prezzoFinale;
+    if (età == "minorenne") {
+        prezzoFinale = (prezzo * 80) / 100;
+        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+    } else if (età == "over") {
+        prezzoFinale = (prezzo * 60) / 100;
+        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+    } else {
+        prezzoFinale = prezzo;
+        document.getElementById("costo_biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+    }
+
+    //
 }
 
-document.getElementById("annulla").addEventListener('click', annullare());
+document.getElementById("annulla").addEventListener('click', annullare);
 
-document.getElementById("genera").addEventListener("click", generare());
+document.getElementById("genera").addEventListener("click", generare);
